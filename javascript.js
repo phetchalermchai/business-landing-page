1521dbddcb7ea1f79142cdf89ae07a8ae2038d1f
Chalermchai Wauwai <59055693+phetchalermchai@users.noreply.github.com>
2024-06-20 22:13:40 +0700

    add script topnav

--- javascript.js
+++ javascript.js
@@ -1,13 +1,33 @@
 function openNav() {
     const mbMenu = document.querySelectorAll(".mobile-menu-container");
     const mbcon = mbMenu[0].getElementsByClassName("mobile-menu-wrap")
+    const bodyOver = document.body
     mbMenu[0].style.height = "100%";
     mbcon[0].style.display = "flex"
+    bodyOver.style.overflowY = "hidden"
 }
 
 function closeNav() {
     const mbMenu = document.querySelectorAll(".mobile-menu-container");
     const mbcon = mbMenu[0].getElementsByClassName("mobile-menu-wrap")
+    const bodyOver = document.body
     mbMenu[0].style.height = "0%";
     mbcon[0].style.display = "none"
+    bodyOver.style.overflowY = "auto"
+}
+
+window.onscroll = function () {
+    scrollFunction()
+}
+
+function scrollFunction() {
+    const navBar = document.querySelectorAll("nav")
+
+    if (document.documentElement.scrollTop > 70) {
+        navBar[0].className = "nav-scroll"
+    } else {
+        navBar[0].className = ""
+    }
+
+    
 }
